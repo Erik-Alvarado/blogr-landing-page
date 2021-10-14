e8c5a2729f78bf4a056f1c84e5b744a84541f481
Tofu <e.alvarado@meditrial.net>
2021-10-14 16:57:26 -0400

    Continued to style our page and working on responsiveness from mobile up.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
 const hamBurger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navbar");
+const innerNav = document.querySelectorAll(".dropdown");
+const dropContent = document.querySelectorAll(".dropdown-content");
 
-hamBurger.addEventListener("click", () => {
+// EventListeners
+
+hamBurger.addEventListener("click", hamburgerToggle);
+innerNav.forEach((inner) =>
+  inner.addEventListener("click", (ev) => innerToggle(ev))
+);
+
+// Functions
+
+function innerToggle(ev) {
+  let clickedItem = ev.target.parentNode;
+  innerNav.forEach((inner) => {
+    if (inner.isSameNode(clickedItem)) {
+      if (inner.classList.contains("open")) {
+        inner.classList.remove("open");
+      } else {
+        inner.classList.add("open");
+      }
+    } else {
+      inner.classList.remove("open");
+    }
+  });
+}
+
+function hamburgerToggle() {
   hamBurger.classList.toggle("active");
   navMenu.classList.toggle("active");
-});
+}
